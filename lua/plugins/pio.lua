2c3a9f1a9a6d747c9fc23d5b3b5498323630aaf7
Beck Boan <beckmboan@gmail.com>
2024-05-19 20:57:35 -0400

    Removed copilot, added markdown, updated onedark

--- lua/plugins/pio.lua
+++ lua/plugins/pio.lua
@@ -1,7 +1,7 @@
 return {
-  { -- Add indentation guides even on blank lines
-    -- 'beckboan/pio.nvim',
-    dir = '~/Dev/repos/pio.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-telescope/telescope.nvim' },
-  },
+	{ -- Add indentation guides even on blank lines
+		-- 'beckboan/pio.nvim',
+		dir = "~/Dev/repos/pio.nvim",
+		dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
+	},
 }
